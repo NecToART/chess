a027060e17268831ec2f03248addb3a0aeb564fe
Artyom Kolbasov <kolbasov.ar@yandex.ru>
2019-04-26 11:06:47 +0300

    Insert function control chess

--- chess/main.swift
+++ chess/main.swift
@@ -10,11 +10,38 @@ import Foundation
 
 typealias Chessman = [String:(alpha:Character,num:Int)?]
 
-var Chessmans = ["Queen":nil,"Lord":(12,1),"Elef":nil] as [String : Any?]
+//____________________________________________________________________________________________________________
+//Проверка на наличие фишки на игровом поле
+//
+func cheesAnalizer(_ Chess: Chessman) -> (){
 
-for (charact,life) in Chessmans{
-    if life == nil{
-        print("DEAD")}
-    else{
-    print(charact)}
+    for (charact,life) in Chess{
+        if life == nil{
+            print("\(charact) DEAD")}
+        else{
+            print(charact,life as Any)}
+    }
 }
+//____________________________________________________________________________________________________________
+//
+//Добавление новой фигуры и обновление старой
+func changeValues( _ Chess:  inout Chessman, _ newChess: Chessman) -> (){
+    for (charact,_) in Chess{
+        for (charactNew,lifeNew) in newChess{
+            if charact != charactNew{
+                Chess[charactNew] = lifeNew
+            } else {
+                Chess[charact] = lifeNew
+                }
+            }
+        }
+    }
+//____________________________________________________________________________________________________________
+
+
+var Chessmans: Chessman = ["Queen":("2",12),"Lord":("1",2),"Elef":nil]
+var newChess: Chessman = ["Elef":("3",7),"PeshkaWhite":(nil),"Peshka":nil]
+
+changeValues(&Chessmans,newChess)
+print(".....................")
+cheesAnalizer(Chessmans)
